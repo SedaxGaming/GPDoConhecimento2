c3a77b141c47887846ecf65eec6e0f6a8625e65b
Guilherme Segalla <bfskylimit@gmail.com>
2022-11-23 20:29:44 -0300

    Atualização da documentação

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -13,19 +13,19 @@ class GameController extends Controller
     }
     public function lancarResposta(Request $request)
     {
-        $input = $request->all();
+        $input = $request->all();//recebe a resposta
         $jaexiste = DB::table('resposta_equipes')
         ->where('idetapas', $input['idEtapa'])
         ->where('idusuarios',session('id'))
         ->where('numero_prova', $input['provaID'])->first();
 
-        if( $jaexiste == null){
+        if( $jaexiste == null){//verifica se ja existe uma resposta da equipe
             Resposta_equipe::insert([
                 'idetapas' => $input['idEtapa'],
                 'idusuarios' => session('id'),
                 'resposta' => $input['respostas'],
                 'numero_prova' => $input['provaID']
-            ]);
+            ]);//processa
             return "Resposta computada com sucesso! Aguarde até a próxima chamada!";
         } else if ($jaexiste != null) {
             return "Uma resposta já foi preenchida para esta etapa, nessa prova! Você deve aguardar até a mesma ser finalizada";
